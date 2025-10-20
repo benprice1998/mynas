ba3af2d4e0a548816059b2a52f23a7455da1914a
benprice1998 <benprice1998@gmail.com>
2025-10-20 16:46:57 -0400

    Updated homepeage layout and features

--- src/main/java/com/example/nas/HomeController.java
+++ src/main/java/com/example/nas/HomeController.java
@@ -5,20 +5,59 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @Autowired
     private StorageService storageService;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+
 
     @GetMapping("/home")
     public String homePage(Model model) {
-        StorageService.DriveInfo driveInfo = storageService.getDriveInfo();
+        StorageService.DriveInfo info = storageService.getDriveInfo();
 
-        model.addAttribute("total", driveInfo.getTotal());
-        model.addAttribute("free", driveInfo.getFree());
-        model.addAttribute("usable", driveInfo.getUsable());
+        long totalBytes = info.getTotal();
+        long freeBytes = info.getFree();
+        long usedBytes = totalBytes - freeBytes;
 
-        return "home"; // Refers to home.html in templates/
+        double totalGB = bytesToGigabytes(totalBytes);
+        double freeGB = bytesToGigabytes(freeBytes);
+        double usedGB = bytesToGigabytes(usedBytes);
+
+        double percentUsed = (usedGB / totalGB) * 100;
+        model.addAttribute("percentUsed", percentUsed);
+
+
+        model.addAttribute("totalRaw", totalGB);
+        model.addAttribute("freeRaw", freeGB);
+        model.addAttribute("usedRaw", usedGB);
+
+        model.addAttribute("total", numberFormat.format(totalGB));
+        model.addAttribute("free", numberFormat.format(freeGB));
+        model.addAttribute("used", numberFormat.format(usedGB));
+
+        List<String> partitions = getMountedPartitions();
+        model.addAttribute("partitions", partitions);
+
+        return "home";
+    }
+
+    private double bytesToGigabytes(long bytes) {
+        return bytes / 1024.0 / 1024.0 / 1024.0;
+    }
+
+    private List<String> getMountedPartitions() {
+        List<String> mounts = new ArrayList<>();
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            mounts.add(root.getAbsolutePath());
+        }
+        return mounts;
     }
 }
